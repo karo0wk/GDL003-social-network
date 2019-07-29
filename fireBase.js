585c2b69da3d7ea1c0f195002049e5a72dc4235c
GalioV <karo0wk@gmail.com>
2019-07-29 16:59:06 -0500

    Solo se muestran botones para iniciar

---
+++ fireBase.js
@@ -0,0 +1,13 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyCq9GGP2yMyHvyyHU34NdIc2u7oDiBzEP8",
+    authDomain: "socialbaica.firebaseapp.com",
+    databaseURL: "https://socialbaica.firebaseio.com",
+    projectId: "socialbaica",
+    storageBucket: "",
+    messagingSenderId: "735431876916",
+    appId: "1:735431876916:web:9953101a112a9262"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+ 
+
---
+++ login.js
@@ -0,0 +1,35 @@
+    var uiConfig = {
+        callbacks: {
+          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            console.log(authResult);
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+          },
+          uiShown: function() {
+            // The widget is rendered.
+            // Hide the loader.
+          }
+        },
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'popup',
+        signInSuccessUrl: 'login.html',
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          firebase.auth.GithubAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: 'login.html',
+        // Privacy policy url.
+        privacyPolicyUrl: '<your-privacy-policy-url>'
+      };
+      let ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
